9a507f883940b42b11f88f988245fbfaf80d87d4
VoldMarius <ukusil@mail.ru>
2024-01-26 09:35:12 +0300

    Finish ALL

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -5,13 +5,8 @@ public class Program {
     public static final String FILE_JSON = "students.json";
     public static final String FILE_XML = "students.xml";
 
-    public static final String FILE_BIN_2 = "students2.bin";
-    public static final String FILE_JSON_2 = "students2.json";
-    public static final String FILE_XML_2 = "students2.xml";
 
     public static void main(String[] args) {
-
-
 
             Student student1 = new Student("Igor", 31, 6.5);
             System.out.println("Создан экземпляр -> " + student1);
